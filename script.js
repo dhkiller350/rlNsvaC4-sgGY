398f859e0e814b49655be1ee0445341ad60f632c
dhkiller350 <100392451+dhkiller350@users.noreply.github.com>
2024-04-08 01:36:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,99 @@ chrome.devtools.network.onRequestFinished.addListener(
 chrome.devtools.network.onRequestFinished.addListener(
   callback: function,
 )
+
+{
+  "manifest_version": 3,
+  "name": "Minimal Manifest",
+  "version": "1.0.0",
+  "description": "A basic example extension with only required keys",
+  "icons": {
+    "48": "images/icon-48.png",
+    "128": "images/icon-128.png"
+  },
+}
+
+{
+  "manifest_version": 3,
+  "name": "Run script automatically",
+  "description": "Runs a script on www.example.com automatically when user installs the extension",
+  "version": "1.0",
+  "icons": {
+    "16": "images/icon-16.png",
+    "32": "images/icon-32.png",
+    "48": "images/icon-48.png",
+    "128": "images/icon-128.png"
+  },
+  "content_scripts": [
+    {
+      "js": [
+        "content-script.js"
+      ],
+      "matches": [
+        "http://*.example.com//"
+      ]
+    }
+  ]
+}
+
+  {
+  "manifest_version": 3,
+  "name": "Side panel extension",
+  "version": "1.0",
+  "description": "Extension with a default side panel.",
+  "icons": {
+    "16": "images/icon-16.png",
+    "48": "images/icon-48.png",
+    "128": "images/icon-128.png"
+  },
+  "side_panel": {
+    "default_path": "sidepanel.html"
+  },
+  "permissions": ["sidePanel"]
+}
+{
+  "manifest_version": 3,
+  "name": "Click to run",
+  "description": "Runs a script when the user clicks the action toolbar icon.",
+  "version": "1.0",
+  "icons": {
+    "16": "images/icon-16.png",
+    "32": "images/icon-32.png",
+    "48": "images/icon-48.png",
+    "128": "images/icon-128.png"
+  },
+  "background": {
+    "service_worker": "service-worker.js"
+  },
+  "action": {
+    "default_icon": {
+      "16": "images/icon-16.png",
+      "32": "images/icon-32.png",
+      "48": "images/icon-48.png",
+      "128": "images/icon-128.png"
+    }
+  },
+  "permissions": ["scripting", "activeTab"]
+}
+
+  {
+  "manifest_version": 3,
+  "name": "Popup extension that requests permissions",
+  "description": "Extension that includes a popup and requests host permissions and storage permissions .",
+  "version": "1.0",
+  "icons": {
+    "16": "images/icon-16.png",
+    "32": "images/icon-32.png",
+    "48": "images/icon-48.png",
+    "128": "images/icon-128.png"
+  },
+  "action": {
+    "default_popup": "popup.html"
+  },
+  "host_permissions": [
+    "https://*.example.com/"
+  ],
+  "permissions": [
+    "storage"
+  ]
+}
